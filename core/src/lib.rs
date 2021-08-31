c084257d927088cc8b48246f998c1224bd6fd720
Nis5l <kirchler123@gmail.com>
2021-08-31 14:44:26 +0200

    fix: bugfixes
    struct without lifetime work
    validator doesnt have to be imported anymore

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,2 +1,68 @@
+//!# Crate for working with Rocket and Json.
+//!
+//! Ultimately the goal is to have validated Structs enter and leave the endpoint as Json
+//! while having everything happen in the background.
+//!
+//!# Example
+//!```
+//!#[macro_use] extern crate rocket;
+//!
+//!#[derive(serde::Deserialize, validator::Validate, rocketjson::JsonBody)]
+//!pub struct RegisterRequest {
+//!   #[validate(length(min = 1))]
+//!   username: String 
+//!}
+//!
+//!#[derive(serde::Serialize)]
+//!pub struct RegisterResponse {
+//!   message: String
+//!}
+//!
+//!#[post("/register", data="<data>")]
+//!pub fn register(data: RegisterRequest) -> rocketjson::ApiResponse<RegisterResponse> {
+//!   rocketjson::ApiResponse::new(rocket::http::Status::Ok, RegisterResponse { message: format!("Welcome {}", data.username) })
+//!}
+//!
+//!#[launch]
+//!fn rocket() -> _ {
+//!    rocket::build()
+//!        .mount("/", routes![register]).
+//!        register("/", vec![rocketjson::error::get_bad_request_catcher()])
+//!}
+//!```
+//!- Input 
+//!```
+//!{
+//!    "username": "testuser"
+//!}
+//!```
+//!- Output 200 OK
+//!```
+//!{
+//!    "message": "Welcome testuser"
+//!}
+//!```
+//!- Input
+//!```
+//!{
+//!    "username": ""
+//!}
+//!```
+//!- Output 400 Bad Request
+//!```
+//!{
+//!    "username": [
+//!        {
+//!            "code": "length",
+//!            "message": null,
+//!            "params": {
+//!                "value": "",
+//!                "min": 1
+//!            }
+//!        }
+//!    ]
+//!}
+//!```
+
 pub use rocketjson_macro::*;
 pub use rocketjson_data::*;
--- macro/src/lib.rs
+++ macro/src/lib.rs
@@ -2,15 +2,19 @@
 
 extern crate proc_macro;
 
-use crate::proc_macro::TokenStream;
 use quote::quote;
-use syn::{DeriveInput, parse_macro_input};
 
 #[proc_macro_derive(JsonBody)]
-pub fn writable_template_derive(input: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(input as DeriveInput);
+pub fn writable_template_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input = syn::parse_macro_input!(input as syn::DeriveInput);
 
     let name = &input.ident;
+    let generics = &input.generics;
+    
+    let mut from_data_generics = generics.clone();
+    if from_data_generics.params.is_empty() {
+        from_data_generics.params.push(syn::parse_quote!('r))
+    }
 
     //TODO:
     //reimplement Error handling once
@@ -19,10 +23,12 @@ pub fn writable_template_derive(input: TokenStream) -> TokenStream {
 
     let expanded = quote! {
         #[rocket::async_trait]
-        impl<'r> rocket::data::FromData<'r> for #name<'r> where Self: validator::Validate {
+        impl #from_data_generics rocket::data::FromData #from_data_generics for #name #generics where Self: validator::Validate {
             type Error = rocketjson::error::JsonBodyError;
 
             async fn from_data(req: &'r rocket::request::Request<'_>, data: rocket::data::Data<'r>) -> rocket::data::Outcome<'r, Self> {
+                use validator::Validate;
+
                 if req.content_type() != Some(&rocket::http::ContentType::new("application", "json")) {
                     return rocket::outcome::Outcome::Forward(data);
                 }
@@ -52,5 +58,5 @@ pub fn writable_template_derive(input: TokenStream) -> TokenStream {
         }
     };
 
-    TokenStream::from(expanded)
+    proc_macro::TokenStream::from(expanded)
 }
