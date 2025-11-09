e34aecb4233ac9cf777b89b803cc380e065afdbd
Nis5l <kirchler123@gmail.com>
2025-11-09 23:17:07 +0100

    fix: rework macro

--- macro/src/lib.rs
+++ macro/src/lib.rs
@@ -3,6 +3,14 @@
 extern crate proc_macro;
 
 use quote::quote;
+use darling::FromDeriveInput;
+
+#[derive(Debug, FromDeriveInput)]
+#[darling(attributes(validate))]
+struct JsonBodyOpts {
+    #[darling(default)]
+    context: Option<syn::Path>,
+}
 
 ///# Validated Json Input
 ///Structs that derive [`JsonBody`] can be used as Endpoint Input.
@@ -28,132 +36,69 @@ use quote::quote;
 ///[`Validator`]: https://github.com/Keats/validator
 ///[`validator::Validate`]: https://docs.rs/validator/0.14.0/validator/trait.Validate.html
 ///[`serde::Deserialize`]: https://docs.serde.rs/serde/trait.Deserialize.html
+#[proc_macro_derive(JsonBody, attributes(validate))]
+pub fn derive_jsonbody(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let ast = syn::parse_macro_input!(input as syn::DeriveInput);
+    let name = &ast.ident;
 
-#[proc_macro_derive(JsonBody)]
-pub fn writable_template_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let mut input = syn::parse_macro_input!(input as syn::DeriveInput);
+    let opts = JsonBodyOpts::from_derive_input(&ast).unwrap();
 
-    input.generics.make_where_clause();
-
-    let name = &input.ident;
-    let mut where_clause = input.generics.where_clause.clone().unwrap();
-
-    let generics = input.generics.clone();
-
-    let mut lgenerics = generics.clone();
-    if lgenerics.params.is_empty() {
-        lgenerics.params.push(syn::parse_quote!('r));
+    let (ctx_line, validate_call) = if let Some(ctx) = opts.context {
+        (
+            // Generate fetching of context from Rocket state
+            quote! {
+                let ctx: & #ctx = req.rocket().state::<#ctx>().expect("context state not found");
+            },
+            // Call validate_with_args if context exists
+            quote! { obj.validate_with_args(ctx) }
+        )
     } else {
-        let mut lgenericsnew = syn::Generics::default();
-        lgenericsnew.params.push(syn::parse_quote!('r));
-        let mut iter = lgenerics.params.iter();
-        iter.next();
-        for g in iter {
-            lgenericsnew.params.push(g.clone());
-        }
-        lgenerics = lgenericsnew;
-    }
-
-    let mut from_data_generics = lgenerics.clone(); 
-    while from_data_generics.params.len() > 1 {
-        from_data_generics.params.pop();
-    }
-
-    let mut validate_args_generics = from_data_generics.clone(); 
-
-    //TODO:
-    //reimplement Error handling once
-    //https://github.com/SergioBenitez/Rocket/issues/749
-    //is out.
-
-    let mut streams = Vec::new();
-
-    let mut args_generics = Vec::new();
-    let mut rocket_states: Vec<syn::Stmt> = Vec::new();
-    let mut rocket_states_variables: Vec<String> = Vec::new();
-
-    for i in 0..10 {
-        if i != 0 {
-            let generic_str = format!("TRJ{}", i);
-            let mut generic = syn::Generics::default();
-            generic.params.push(syn::parse_str(&generic_str).unwrap());
-
-            rocket_states.push(syn::parse_str(&format!("let p{} = req.rocket().state::<{}>().expect(\"type not found in state\");", i, generic_str)[..]).unwrap());
-            rocket_states_variables.push(format!("p{}", i));
-
-            args_generics.push(generic_str.clone());
-
-            lgenerics.params.push(syn::parse_str(&format!("{}: 'static", generic_str)[..]).unwrap());
-
-        }
-
-        validate_args_generics.params.push(
-            syn::parse_str(&format!("Args=({})", args_generics.iter().map(|s| { format!("&'r {}", s) }).collect::<Vec<String>>().join(","))).unwrap()
-        );
-
-        where_clause.predicates = syn::punctuated::Punctuated::new();
-        where_clause.predicates.push(syn::parse_quote!(Self: validator::ValidateArgs#validate_args_generics));
-        for g in args_generics.iter() {
-            where_clause.predicates.push(syn::parse_str(&format!("{}: Sync + std::marker::Send", g)[..]).unwrap());
-        }
-
-        let rocket_states_folded = rocket_states.iter().fold(quote! {}, |acc, new| quote! {#acc #new});
-        let rocket_states_variables: syn::Type = syn::parse_str(&format!("({})", rocket_states_variables.join(","))[..]).unwrap();
-
-        streams.push(quote! {
-            #[rocket::async_trait]
-            //impl #from_data_generics rocket::data::FromData #from_data_generics for #name #generics where Self: validator::Validate {
-            impl #lgenerics rocket::data::FromData #from_data_generics for #name #generics
-                #where_clause
-                {
-
-                type Error = ();
-
-                async fn from_data(req: &'r rocket::request::Request<'_>, data: (rocket::data::Data<'r>, rocket:http::Status)) -> rocket::data::Outcome<'r, Self> {
-                    use validator::ValidateArgs;
-
-                    if req.content_type() != Some(&rocket::http::ContentType::new("application", "json")) {
-                        return rocket::outcome::Outcome::Forward(data);
-                    }
-
-                    let json_opt = rocket::serde::json::Json::<Self>::from_data(req, data.0).await;
-
-                    //TODO:
-                    //let opt =
-                    match json_opt {
-                        rocket::outcome::Outcome::Failure(_) => {
-                            req.local_cache(|| rocketjson::error::JsonBodyError::JsonValidationError);
-                            return rocket::outcome::Outcome::Error((rocket::http::Status::BadRequest, ()));
-                        },
-                        rocket::outcome::Outcome::Forward(forward) => {
-                            return rocket::outcome::Outcome::Forward(forward);
-                        },
-                        rocket::outcome::Outcome::Success(_) => ()
-                    }
-
-                    let obj = json_opt.unwrap().0;
-
-                    #rocket_states_folded
-                    let errors_ok = obj.validate_args(#rocket_states_variables) ;
-
-                    if let Err(errors) = errors_ok {
-                        req.local_cache(move || rocketjson::error::JsonBodyError::ValidationError(errors));
-                        return rocket::outcome::Outcome::Error((rocket::http::Status::BadRequest, ()))
-                    }
-
-                    rocket::outcome::Outcome::Success(obj)
-                }
-            }
-        });
-
-        validate_args_generics.params.pop();
-    }
-
-    let folded = streams.iter().fold(quote! {}, |acc, new| quote! {#acc #new});
-    
-    let stream = quote! {
-        #folded 
+        (
+            // No context line
+            quote! {},
+            // Call normal validate
+            quote! { obj.validate() }
+        )
     };
-    
-    proc_macro::TokenStream::from(stream)
+
+    let gen = quote! {
+        #[rocket::async_trait]
+        impl<'r> rocket::data::FromData<'r> for #name {
+            type Error = ();
+
+            async fn from_data(
+                req: &'r rocket::Request<'_>,
+                data: rocket::data::Data<'r>
+            ) -> rocket::data::Outcome<'r, Self> {
+                //NOTE: forward if not JSON
+                if req.content_type() != Some(&rocket::http::ContentType::new("application", "json")) {
+                    return rocket::data::Outcome::Forward((data, rocket::http::Status::Continue));
+                }
+
+                //NOTE: parse JSON
+                let json_outcome = rocket::serde::json::Json::<Self>::from_data(req, data).await;
+
+                let obj = match json_outcome {
+                    rocket::data::Outcome::Success(json) => json.into_inner(),
+                    rocket::data::Outcome::Error(_) => {
+                        req.local_cache(|| rocketjson::error::JsonBodyError::JsonValidationError);
+                        return rocket::data::Outcome::Error((rocket::http::Status::BadRequest, ()));
+                    },
+                    rocket::data::Outcome::Forward(f) => return rocket::data::Outcome::Forward(f),
+                };
+
+                //NOTE: validate
+                #ctx_line
+
+                if let Err(errors) = #validate_call {
+                    req.local_cache(|| rocketjson::error::JsonBodyError::ValidationError(errors));
+                    return rocket::data::Outcome::Error((rocket::http::Status::BadRequest, ()));
+                }
+
+                rocket::data::Outcome::Success(obj)
+            }
+        }
+    };
+
+    gen.into()
 }
